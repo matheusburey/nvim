467a333ce5eddff5cd8a805fa6428707f2b47463
Matheus-Henrique-Burey <matheushburei.99@gmail.com>
2022-03-17 15:49:32 -0300

    refactor: ornanizations plugins

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
 require('settings')
 require('keymaps')
 require('plugins')
-
---PLUGINS--
-require('telescope')
-
-require('plugins.nvim-tree')
-require('plugins.dashboard')
-require('plugins.treesitter')
-require('plugins.feline')
-require("plugins.signature")
-require("plugins.lspconfig") -- Install server: pip install pynvim
-require("plugins.cmp")
-
-require('nvim-autopairs').setup({disable_filetype = { "TelescopePrompt" , "vim" }})
-require('nvim-ts-autotag').setup()
-require("bufferline").setup()
