a35e457f44034aa3fbeedf5c94cfa4a7cd5e981b
Matheus-Henrique-Burey <matheushburei99@gmail.com>
2022-03-29 12:19:35 -0300

    refactor: changing to standard baffur configuration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,7 @@ require("telescope")
 require("nvim-tree").setup()
 require("nvim-ts-autotag").setup()
 require('gitsigns').setup()
+require("bufferline").setup()
 
 require("plugins.dashboard")
 require("plugins.treesitter")
@@ -13,4 +14,3 @@ require("plugins.signature")
 require("plugins.lspconfig") -- Install server: pip install pynvim
 require("plugins.cmp")
 require("plugins.autopairs")
-require("plugins.bufferline")
