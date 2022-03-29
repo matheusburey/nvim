ed624670846c3ae12be19673043b66b525c0b7e3
Matheus-Henrique-Burey <matheushburei99@gmail.com>
2022-03-29 20:13:56 -0300

    feat: add keymap better window navigation and resize with arrows

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,10 +14,10 @@ local cmd = vim.cmd
 -- map Esc to kk
 map("i", "kk", "<Esc>", {noremap = true})
 
--- fast saving with <leader> and s
+-- fast saving with ctrl  and s
 map("n", "<C-s>", ":write<CR>", default_opts)
 
--- fast saving quit <leader> and q
+-- fast saving quit ctrl and q
 map("n", "<C-q>", ":quit<CR>", default_opts)
 
 -- Move text up and down
@@ -27,12 +27,25 @@ map("n", "<M-j>", ":m .+1<CR>", default_opts)
 map("x", "<M-k>", ":m '<-2<CR>gv-gv", default_opts)
 map("x", "<M-j>", ":m '>+1<CR>gv-gv", default_opts)
 
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h", default_opts)
+map("n", "<C-j>", "<C-w>j", default_opts)
+map("n", "<C-k>", "<C-w>k", default_opts)
+map("n", "<C-l>", "<C-w>l", default_opts)
+
+-- Resize with arrows
+map("n", "<C-Up>", ":resize -2<CR>", default_opts)
+map("n", "<C-Down>", ":resize +2<CR>", default_opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", default_opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", default_opts)
+
 -- nvim-tree
-map("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)       -- open/close
+map("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)
 
 -- Navigate buffers
 map("n", "<M-l>", ":bnext<CR>", default_opts)
 map("n", "<M-h>", ":bprevious<CR>", default_opts)
 map("n", "<M-c>", ":bdelet<CR>", default_opts)
 
+-- Open Telescope
 map("n", "<M-g>", ":Telescope<CR>", default_opts)
