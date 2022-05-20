49016b1ecb9dd5484db47b92d77383cd0f3515cb
matheusburey <matheushburei@gmail.com>
2022-05-20 14:42:34 -0300

    refactor: import feline function deprecated

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -1,220 +1,192 @@
-if not pcall(require, "feline") then
-  return
+local present, feline = pcall(require, "feline")
+
+if not present then
+	return
 end
-
-local colors = {
-  bg = '#282c34',
-  fg = '#abb2bf',
-  yellow = '#e0af68',
-  cyan = '#56b6c2',
-  darkblue = '#081633',
-  green = '#98c379',
-  orange = '#d19a66',
-  violet = '#a9a1e1',
-  magenta = '#c678dd',
-  blue = '#61afef',
-  red = '#e86671'
-}
-
-local vi_mode_colors = {
-  NORMAL = colors.green,
-  INSERT = colors.red,
-  VISUAL = colors.magenta,
-  OP = colors.green,
-  BLOCK = colors.blue,
-  REPLACE = colors.violet,
-  ['V-REPLACE'] = colors.violet,
-  ENTER = colors.cyan,
-  MORE = colors.cyan,
-  SELECT = colors.orange,
-  COMMAND = colors.green,
-  SHELL = colors.green,
-  TERM = colors.green,
-  NONE = colors.yellow
-}
 
 local lsp = require 'feline.providers.lsp'
 local lsp_severity = vim.diagnostic.severity
 local vi_mode_utils = require 'feline.providers.vi_mode'
 
-local lsp_get_diag = function(str)
-  local count = vim.lsp.diagnostic.get_count(0, str)
-  return (count > 0) and ' '..count..' ' or ''
-end
+local colors = {
+	bg = '#282c34',
+	fg = '#abb2bf',
+	yellow = '#e0af68',
+	cyan = '#56b6c2',
+	darkblue = '#081633',
+	green = '#98c379',
+	orange = '#d19a66',
+	violet = '#a9a1e1',
+	magenta = '#c678dd',
+	blue = '#61afef',
+	red = '#e86671'
+}
 
 -- LuaFormatter off
-
 local comps = {
-  vi_mode = {
-    left = {
-      provider = function()
-        return vi_mode_utils.get_vim_mode()
-      end,
-      hl = function()
-        local val = {
-          name = vi_mode_utils.get_mode_highlight_name(),
-          fg = vi_mode_utils.get_mode_color(),
-          -- fg = colors.bg
-        }
-        return val
-      end,
-      right_sep = ' '
-    },
-    right = {
-      provider = '  Linux' ,
-      hl = function()
-        local val = {
-          name = vi_mode_utils.get_mode_highlight_name(),
-          fg = vi_mode_utils.get_mode_color()
-        }
-        return val
-      end,
-      left_sep = ' ',
-      right_sep = ' '
-    }
-  },
-  file = {
-    info = {
-      provider = {
-        name = 'file_info',
-        opts = {
-          type = 'relative',
-          file_readonly_icon = '  ',
-          file_modified_icon = '',
-        }
-      },
-      hl = {
-        fg = colors.blue,
-        style = 'bold'
-      }
-    },
-    encoding = {
-      provider = 'file_encoding',
-      left_sep = ' ',
-      hl = {
-        fg = colors.violet,
-        style = 'bold'
-      }
-    },
-    type = {
-      provider = 'file_type'
-    },
-    position = {
-      provider = 'position',
-      left_sep = ' ',
-      hl = {
-        fg = colors.cyan,
-      }
-    },
-  },
-  left_end = {
-    provider = function() return '' end,
-    hl = {
-      fg = colors.bg,
-      bg = colors.blue,
-    }
-  },
-  scroll_bar = {
-    provider = 'scroll_bar',
-    left_sep = ' ',
-    hl = {
-      fg = colors.blue,
-      style = 'bold'
-    }
-  },
-  diagnos = {
-    err = {
-      -- provider = 'diagnostic_errors',
-      provider = function()
-        return '' .. lsp_get_diag("Error")
-      end,
-      -- left_sep = ' ',
-      enabled = function() return lsp.diagnostics_exist(lsp_severity.ERROR) end,
-      hl = {
-        fg = colors.red
-      }
-    },
-    warn = {
-      -- provider = 'diagnostic_warnings',
-      provider = function()
-        return '' ..  lsp_get_diag("Warning")
-      end,
-      -- left_sep = ' ',
-      enabled = function() return lsp.diagnostics_exist(lsp_severity.WARN) end,
-      hl = {
-        fg = colors.yellow
-      }
-    },
-    info = {
-      -- provider = 'diagnostic_info',
-      provider = function()
-        return '' .. lsp_get_diag("Information")
-      end,
-      -- left_sep = ' ',
-      enabled = function() return lsp.diagnostics_exist(lsp_severity.INFO) end,
-      hl = {
-        fg = colors.blue
-      }
-    },
-    hint = {
-      -- provider = 'diagnostic_hints',
-      provider = function()
-        return '' .. lsp_get_diag("Hint")
-      end,
-      -- left_sep = ' ',
-      enabled = function() return lsp.diagnostics_exist(lsp_severity.HINT) end,
-      hl = {
-        fg = colors.cyan
-      }
-    },
-  },
-  lsp = {
-    name = {
-      provider = 'lsp_client_names',
-      -- left_sep = ' ',
-      right_sep = ' ',
-      icon = '  ',
-      -- icon = '慎',
-      hl = {
-        fg = colors.yellow
-      }
-    }
-  },
-  git = {
-    branch = {
-      provider = 'git_branch',
-      icon = ' ',
-      -- icon = ' ',
-      left_sep = ' ',
-      hl = {
-        fg = colors.violet,
-        style = 'bold'
-      },
-    },
-    add = {
-      provider = 'git_diff_added',
-      hl = {
-        fg = colors.green
-      }
-    },
-    change = {
-      provider = 'git_diff_changed',
-      hl = {
-        fg = colors.orange
-      }
-    },
-    remove = {
-      provider = 'git_diff_removed',
-      hl = {
-        fg = colors.red
-      }
-    }
-  }
+	vi_mode = {
+		left = {
+			provider = function()
+				return vi_mode_utils.get_vim_mode()
+			end,
+			hl = function()
+				local val = {
+					name = vi_mode_utils.get_mode_highlight_name(),
+					fg = vi_mode_utils.get_mode_color(),
+					-- fg = colors.bg
+				}
+				return val
+			end,
+			right_sep = ' '
+		},
+		right = {
+			provider = '  Linux',
+			hl = function()
+				local val = {
+					name = vi_mode_utils.get_mode_highlight_name(),
+					fg = vi_mode_utils.get_mode_color()
+				}
+				return val
+			end,
+			left_sep = ' ',
+			right_sep = ' '
+		}
+	},
+	file = {
+		info = {
+			provider = {
+				name = 'file_info',
+				opts = {
+					type = 'relative',
+					file_readonly_icon = '  ',
+					file_modified_icon = '',
+				}
+			},
+			hl = {
+				fg = colors.blue,
+				style = 'bold'
+			}
+		},
+		encoding = {
+			provider = 'file_encoding',
+			left_sep = ' ',
+			hl = {
+				fg = colors.violet,
+				style = 'bold'
+			}
+		},
+		type = {
+			provider = 'file_type'
+		},
+		position = {
+			provider = 'position',
+			left_sep = ' ',
+			hl = {
+				fg = colors.cyan,
+			}
+		},
+	},
+	left_end = {
+		provider = function() return '' end,
+		hl = {
+			fg = colors.bg,
+			bg = colors.blue,
+		}
+	},
+	scroll_bar = {
+		provider = 'scroll_bar',
+		left_sep = ' ',
+		hl = {
+			fg = colors.blue,
+			style = 'bold'
+		}
+	},
+	lsp = {
+		name = {
+			provider = 'lsp_client_names',
+			-- left_sep = ' ',
+			right_sep = ' ',
+			icon = '  ',
+			-- icon = '慎',
+			hl = {
+				fg = colors.yellow
+			}
+		}
+	},
+	git = {
+		branch = {
+			provider = 'git_branch',
+			icon = ' ',
+			-- icon = ' ',
+			left_sep = ' ',
+			hl = {
+				fg = colors.violet,
+				style = 'bold'
+			},
+		},
+		add = {
+			provider = 'git_diff_added',
+			hl = {
+				fg = colors.green
+			}
+		},
+		change = {
+			provider = 'git_diff_changed',
+			hl = {
+				fg = colors.orange
+			}
+		},
+		remove = {
+			provider = 'git_diff_removed',
+			hl = {
+				fg = colors.red
+			}
+		}
+	}
+}
+
+local diagnostic = {
+	err = {
+		provider = 'diagnostic_errors',
+		left_sep = ' ',
+		enabled = function() return lsp.diagnostics_exist(lsp_severity.ERROR) end,
+		hl = {
+			fg = colors.red,
+			icon = ''
+		}
+	},
+	warn = {
+		provider = 'diagnostic_warnings',
+		left_sep = ' ',
+		enabled = function() return lsp.diagnostics_exist(lsp_severity.WARN) end,
+		hl = {
+			fg = colors.yellow,
+			icon = ''
+		}
+	},
+	info = {
+		provider = 'diagnostic_info',
+		left_sep = ' ',
+		enabled = function() return lsp.diagnostics_exist(lsp_severity.INFO) end,
+		hl = {
+			fg = colors.blue,
+			icon = ''
+		}
+	},
+	hint = {
+		provider = 'diagnostic_hints',
+		left_sep = ' ',
+		enabled = function() return lsp.diagnostics_exist(lsp_severity.HINT) end,
+		hl = {
+			fg = colors.cyan,
+			icon = ''
+		}
+	}
 }
 
 local components = {
-  active = {{}, {}, {}},
-  inactive = {{}, {}, {}},
+	active = { {}, {}, {} },
+	inactive = { {}, {}, {} },
 }
 
 table.insert(components.active[1], comps.vi_mode.left)
@@ -223,29 +195,21 @@ table.insert(components.active[1], comps.git.branch)
 table.insert(components.active[1], comps.git.add)
 table.insert(components.active[1], comps.git.change)
 table.insert(components.active[1], comps.git.remove)
-table.insert(components.inactive[1], comps.vi_mode.left)
-table.insert(components.inactive[1], comps.file.info)
-table.insert(components.active[3], comps.diagnos.err)
-table.insert(components.active[3], comps.diagnos.warn)
-table.insert(components.active[3], comps.diagnos.hint)
-table.insert(components.active[3], comps.diagnos.info)
+
+table.insert(components.active[3], diagnostic.err)
+table.insert(components.active[3], diagnostic.warn)
+table.insert(components.active[3], diagnostic.hint)
+table.insert(components.active[3], diagnostic.info)
+
 table.insert(components.active[3], comps.lsp.name)
 table.insert(components.active[3], comps.file.position)
 table.insert(components.active[3], comps.scroll_bar)
 table.insert(components.active[3], comps.vi_mode.right)
 
-require'feline'.setup {
-  colors = { bg = colors.bg, fg = colors.fg },
-  components = components,
-  vi_mode_colors = vi_mode_colors,
-  force_inactive = {
-    filetypes = {
-      'packer',
-      'NvimTree',
-      'fugitive',
-      'fugitiveblame'
-    },
-    buftypes = {'terminal'},
-    bufnames = {}
-  }
+table.insert(components.inactive[1], comps.vi_mode.left)
+table.insert(components.inactive[1], comps.file.info)
+
+feline.setup {
+	colors = { bg = colors.bg, fg = colors.fg },
+	components = components,
 }
