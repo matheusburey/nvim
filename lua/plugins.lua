a5b2bb83e94a8e4bbaee0f147cfeb2c47833d20f
matheusburey <matheushburei.99@gmail.com>
2022-05-26 10:42:57 -0300

    refactor: add validation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,15 @@
 require("plugins.packer")
 --PLUGINS--
-require("nvim-tree").setup()
-require("nvim-ts-autotag").setup()
-require("gitsigns").setup()
-require("bufferline").setup()
-require("Comment").setup()
-require("lsp_signature").setup()
 
-require("plugins.lspconfig").setup()
+local plugins = { "nvim-tree", "nvim-ts-autotag", "gitsigns", "bufferline", "Comment", "lsp_signature", "plugins.lspconfig" }
+
+for _, plu in pairs(plugins) do
+  local present, plugin = pcall(require, plu)
+  if present then
+    plugin.setup()
+  end
+end
+
 require("plugins.autopairs")
 require("plugins.dashboard")
 require("plugins.feline")
