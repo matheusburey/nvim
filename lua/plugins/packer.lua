44d74a0319adabec46f1aed64e70ac29e1e2d24c
matheusburey <matheushburei.99@gmail.com>
2022-06-04 17:11:33 -0300

    refactor(packer): remove line error

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,7 +1,5 @@
 -- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
 -- Automatically install packer
 local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
