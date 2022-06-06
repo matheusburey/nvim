c1b786c7d460d80e780012ae27879d63a3e0e651
matheusburey <matheushburei.99@gmail.com>
2022-06-06 15:06:11 -0300

    feat: alteration formating to async

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -76,7 +76,7 @@ local function lsp_keymaps(bufnr)
   -- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-f>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-f>", "<cmd>lua vim.lsp.buf.format({async=true})<CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
