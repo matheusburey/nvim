d0047f2c78fcd402df497a2417d703aa0995f60c
matheusburey <matheushburei.99@gmail.com>
2022-07-12 20:34:52 -0300

    feat(plugins/dashboard): add colors in the header

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,25 +3,12 @@ if not status_ok then
   return
 end
 
+local home = os.getenv('HOME')
 vim.g.dashboard_default_executive = "telescope"
-
-db.custom_header = {
-  "                                   ",
-  "                                   ",
-  "                                   ",
-  "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-  "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-  "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-  "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-  "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-  "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-  "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-  " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-  " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-  "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-  "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-  "                                   ",
-}
+db.preview_command = 'cat | lolcat -F 0.3'
+db.preview_file_path = home .. '/.config/nvim/neovim.cat'
+db.preview_file_height = 15
+db.preview_file_width = 35
 
 db.custom_center = {
   { icon = '  ', desc = 'Recents                   ', shortcut = 'SPC f o', action = 'Telescope oldfiles' },
