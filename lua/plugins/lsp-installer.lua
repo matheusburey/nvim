0520289f70accf7c15927ac03851396c56ce0a57
matheusburey <matheushburei.99@gmail.com>
2022-08-21 15:41:04 -0300

    feat: config emmet_ls

--- lua/plugins/lsp-installer.lua
+++ lua/plugins/lsp-installer.lua
@@ -1,102 +1,88 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+  return
 end
 
 local status, schemastore = pcall(require, "schemastore")
 if not status then
-	return
+  return
 end
 
 local config_lsp = {
-	sumneko_opts = {
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-				workspace = {
-					library = {
-						[vim.fn.expand "$VIMRUNTIME/lua"] = true,
-						[vim.fn.stdpath "config" .. "/lua"] = true,
-					},
-				},
-			},
-		},
-	},
-	emmet_opts= {
-		cmd = { "ls_emmet", "--stdio" },
-		filetypes = {
-			"html",
-			"css",
-			"scss",
-			"haml",
-			"xsl",
-			"pug",
-			"slim",
-			"sass",
-			"stylus",
-			"less",
-			"sss",
-			"hbs",
-			"handlebars",
-		},
-	},
-	jsonls_opts = {
-		init_options = {
-			provideFormatter = false,
-		},
-		settings = {
-			json = {
-				schemas = schemastore.json.schemas(),
-			},
-		},
-		setup = {
-			commands = {
-				Format = {
-					function()
-						vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-					end,
-				},
-			},
-		},
-	},
-	pyright_opts = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				useLibraryCodeForTypes = true
-			}
-		}
-	}
+  sumneko_opts = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+            [vim.fn.stdpath "config" .. "/lua"] = true,
+          },
+        },
+      },
+    },
+  },
+  emmet_opts = {
+    capabilities = vim.lsp.protocol.make_client_capabilities(),
+    filetypes = { 'html', 'typescriptreact', 'javascrip', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+  },
+  jsonls_opts = {
+    init_options = {
+      provideFormatter = false,
+    },
+    settings = {
+      json = {
+        schemas = schemastore.json.schemas(),
+      },
+    },
+    setup = {
+      commands = {
+        Format = {
+          function()
+            vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+          end,
+        },
+      },
+    },
+  },
+  pyright_opts = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true
+      }
+    }
+  }
 }
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("plugins.lspconfig").on_attach,
-		capabilities = require("plugins.lspconfig").capabilities,
-	}
+  local opts = {
+    on_attach = require("plugins.lspconfig").on_attach,
+    capabilities = require("plugins.lspconfig").capabilities,
+  }
 
-	if server.name == "jsonls" then
-		opts = vim.tbl_deep_extend("force", config_lsp.jsonls_opts, opts)
-	end
+  if server.name == "jsonls" then
+    opts = vim.tbl_deep_extend("force", config_lsp.jsonls_opts, opts)
+  end
 
-	if server.name == "sumneko_lua" then
-		opts = vim.tbl_deep_extend("force", config_lsp.sumneko_opts, opts)
-	end
+  if server.name == "sumneko_lua" then
+    opts = vim.tbl_deep_extend("force", config_lsp.sumneko_opts, opts)
+  end
 
-	if server.name == "pyright" then
-		opts = vim.tbl_deep_extend("force", config_lsp.pyright_opts, opts)
-	end
+  if server.name == "pyright" then
+    opts = vim.tbl_deep_extend("force", config_lsp.pyright_opts, opts)
+  end
 
-	if server.name == "emmet_ls" then
-		opts = vim.tbl_deep_extend("force", config_lsp.emmet_opts, opts)
-	end
+  if server.name == "emmet_ls" then
+    opts = vim.tbl_deep_extend("force", config_lsp.emmet_opts, opts)
+  end
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
 end)
