a6c1ba3cbdc44a7d9580cb017a0d90005c8c6802
matheusburey <matheushburei.99@gmail.com>
2023-01-27 15:16:22 -0300

    fix update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,16 +3,60 @@ if not status_ok then
   return
 end
 
-local home = os.getenv('HOME')
 vim.g.dashboard_default_executive = "telescope"
-db.preview_command = 'cat | lolcat -F 0.3'
-db.preview_file_path = home .. '/.config/nvim/neovim.cat'
-db.preview_file_height = 10
-db.preview_file_width = 55
 
-db.custom_center = {
-  { icon = '  ', desc = 'Recents                   ', shortcut = 'SPC f o', action = 'Telescope oldfiles' },
-  { icon = '  ', desc = 'Find File                 ', shortcut = 'SPC f f', action = 'Telescope find_files' },
-  { icon = '  ', desc = 'Find Word                 ', shortcut = 'SPC f w', action = 'Telescope live_grep' },
-  { icon = '  ', desc = 'New File                  ', shortcut = 'SPC f n', action = 'DashboardNewFile' },
-}
+db.setup({
+  theme = 'doom', --  theme is doom and hyper default is hyper
+  config = {
+    header = {
+      "",
+      "",
+      "███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+      "████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+      "██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+      "██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+      "██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+      "╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+      "",
+      "",
+    },
+    center = {
+      {
+        icon = '  ',
+        icon_hl = {fg = "#ff0000"},
+        desc = 'Recents                   ',
+        desc_hl = {fg = "#ff0000"},
+        key = 'SPC f o',
+        key_hl = {fg = "#ff0000"},
+        action = 'Telescope oldfiles'
+      },
+      {
+        icon = '  ',
+        icon_hl = {fg = "#ff0000"},
+        desc = 'Find File                 ',
+        desc_hl = {fg = "#ff0000"},
+        key = 'SPC f f',
+        key_hl = {fg = "#ff0000"},
+        action = 'Telescope find_files'
+      },
+      {
+        icon = '  ',
+        icon_hl = {fg = "#ff0000"},
+        desc = 'Find Word                 ',
+        desc_hl = {fg = "#ff0000"},
+        key = 'SPC f w',
+        key_hl = {fg = "#ff0000"},
+        action = 'Telescope live_grep'
+      },
+      {
+        icon = '  ',
+        icon_hl = {fg = "#ff0000"},
+        desc = 'New File                  ',
+        desc_hl = {fg = "#ff0000"},
+        key = 'SPC f n',
+        key_hl = {fg = "#ff0000"},
+        action = 'DashboardNewFile'
+      },
+    },
+  }
+})
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -4,104 +4,103 @@
 local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system {
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	}
-	print "Installing packer close and reopen Neovim..."
-	vim.cmd [[packadd packer.nvim]]
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
 packer.init {
-	display = {
-		open_fn = function()
-			return require("packer.util").float { border = "rounded" }
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
 }
 
-local use = require("packer").use
-return packer.startup(function()
-	-- Packer can manage itself
-	use "wbthomason/packer.nvim"
+return packer.startup(function(use)
+  -- Packer can manage itself
+  use "wbthomason/packer.nvim"
 
   -- provides framework for easy foldtext customization
   use "kevinhwang91/nvim-ufo"
   use "kevinhwang91/promise-async"
 
-	-- Packer devicons
-	use "kyazdani42/nvim-web-devicons"
+  -- Packer devicons
+  use "nvim-tree/nvim-web-devicons"
 
-	-- Packer dashboard
-	use "glepnir/dashboard-nvim"
+  -- Packer dashboard
+  use { "glepnir/dashboard-nvim", event = 'VimEnter' }
 
-	-- Packer file explorer
-	use "kyazdani42/nvim-tree.lua"
+  -- Packer file explorer
+  use "kyazdani42/nvim-tree.lua"
 
-	-- Packer terminal
+  -- Packer terminal
   use "akinsho/toggleterm.nvim"
 
-	-- Packer smart commenting
-	use "numToStr/Comment.nvim"
+  -- Packer smart commenting
+  use "numToStr/Comment.nvim"
 
-	-- Packer statusline
-	use "feline-nvim/feline.nvim"
+  -- Packer statusline
+  use "feline-nvim/feline.nvim"
 
-	-- Packer theme dracula
-	use "Mofiqul/dracula.nvim"
+  -- Packer theme dracula
+  use "Mofiqul/dracula.nvim"
 
-	-- Packer autopairs
-	use "windwp/nvim-autopairs"
+  -- Packer autopairs
+  use "windwp/nvim-autopairs"
 
-	-- Packer git decorations
-	use "lewis6991/gitsigns.nvim"
+  -- Packer git decorations
+  use "lewis6991/gitsigns.nvim"
 
-	-- Packer configurations and abstraction layer for Neovim
-	use "nvim-treesitter/nvim-treesitter"
-	use "p00f/nvim-ts-rainbow"
-	use "nvim-treesitter/playground"
+  -- Packer configurations and abstraction layer for Neovim
+  use "nvim-treesitter/nvim-treesitter"
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
 
-	-- Packer autoclose and autorename html tag
-	use "windwp/nvim-ts-autotag"
+  -- Packer autoclose and autorename html tag
+  use "windwp/nvim-ts-autotag"
 
-	-- Packer telescope
-	use "nvim-telescope/telescope.nvim"
-	use "nvim-lua/plenary.nvim"
+  -- Packer telescope
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-lua/plenary.nvim"
 
-	-- Packer bufferline
-	use "akinsho/bufferline.nvim"
+  -- Packer bufferline
+  use "akinsho/bufferline.nvim"
 
-	-- Packer nvim-cmp
-	use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/cmp-buffer"
-	use "hrsh7th/cmp-path"
-	use "hrsh7th/cmp-cmdline"
+  -- Packer nvim-cmp
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-cmdline"
 
-	-- snippets
-	use "L3MON4D3/LuaSnip"
-	use "saadparwaiz1/cmp_luasnip"
-	use "rafamadriz/friendly-snippets"
+  -- snippets
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+  use "rafamadriz/friendly-snippets"
 
-	-- LSP plugins
-	use "neovim/nvim-lspconfig" -- Packer lspconf
-	use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-	use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  -- LSP plugins
+  use "neovim/nvim-lspconfig" -- Packer lspconf
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
-	-- Packer Show function signature when you type
-	use "ray-x/lsp_signature.nvim"
+  -- Packer Show function signature when you type
+  use "ray-x/lsp_signature.nvim"
 
-	-- Packer Neovim Lua plugin providing access to the SchemaStore catalog.
-	use "b0o/SchemaStore.nvim"
+  -- Packer Neovim Lua plugin providing access to the SchemaStore catalog.
+  use "b0o/SchemaStore.nvim"
 end)
